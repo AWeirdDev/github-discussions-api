f092ab8069eaf4f8761e483b772901fa377d24f4
JC <90096971+AWeirdScratcher@users.noreply.github.com>
2024-04-04 20:43:58 +0800

    Delete api directory

--- api/index.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get('/')
-async def index():
-    return { "message": "Hello, World!", "url": "https://github.com/AWeirdScratcher/github-discussions-api" }
